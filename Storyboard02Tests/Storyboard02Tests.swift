34af5a415aed818ae1cdf0351a99b348e6d58a17
Seungpyo <rush94434@gmail.com>
2025-10-14 11:25:01 +0900

    Initial Commit

---
+++ Storyboard02Tests/Storyboard02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Storyboard02Tests.swift
+//  Storyboard02Tests
+//
+//  Created by 홍승표 on 10/14/25.
+//
+
+import Testing
+@testable import Storyboard02
+
+struct Storyboard02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
